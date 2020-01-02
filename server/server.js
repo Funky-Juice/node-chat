69c6a42f5f0c6e7ef3adfc819275906646ac6ea3
Funky-Juice <isa86box@gmail.com>
2020-01-02 14:37:18 +0300

    подключение пользователя

--- server/server.js
+++ server/server.js
@@ -10,16 +10,24 @@ const app = express()
 const server = http.createServer(app)
 const io = socketIO(server)
 
-const message = (name, text) => ({name, text})
+const message = (name, text, id) => ({name, text, id})
 
 io.on('connection', (socket) => {
+  socket.on('join', (user, callback) => {
+    if (!user.name || !user.room) {
+      return callback(`Enter valid user data`)
+    }
+
+    callback({userId: socket.id})
+    socket.emit('message: new', message('Admin', `Welcome, ${user.name}!`))
+  })
 
   socket.on('message: create', (data, callback) => {
     if (!data) {
       callback(`Message can't be empty`)
     } else {
       callback()
-      io.emit('message: new', message('Admin', data.text))
+      io.emit('message: new', message(data.name, data.text, data.id))
     }
   })
 })
